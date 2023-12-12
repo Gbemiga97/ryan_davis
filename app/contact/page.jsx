cb6105c15d48ec3a46df2b68f7a6d193ff0a5191
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-12-12 17:21:44 +0100

    final commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         {/* text & illustration */}
         <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
           {/* text */}
-          <div className="flex flex-col justify-center">
+          <div className="flex flex-col justify-center text-center xl:text-left items-center xl:items-start">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
             <span className="w-[1.8rem] h-[2px] bg-primary"/>
             Say Hello
@@ -27,14 +27,14 @@ const Contact = () => {
         {/* info, text & form */}
         <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
           {/* info text */}
-          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg mx-auto xl:mx-0">
             {/* mail */}
             <div className="flex items-center gap-x-8">
               <MailIcon size={18} className="text-primary" />
               <p>youremail@gmail.com</p>
             </div>
              {/* address */}
-             <div className="flex items-center gap-x-8">
+             <div className="flex items-center gap-x-8 ">
               <HomeIcon size={18} className="text-primary" />
               <p>8420 pine Avenue, Meadowville MC 842</p>
             </div>
